4c0b04b55d3a73183247e47ad99362b374ed4ec3
Can Undeger <canundeger@gmail.com>
2018-07-03 00:21:18 -0400

    Implement postLiveEvent function
    - This function lets you to post a regular event
    with the given tag, and creates an event if
    necessary.

--- app/src/main/java/com/insac/can/livebus/core/LiveBus.kt
+++ app/src/main/java/com/insac/can/livebus/core/LiveBus.kt
@@ -3,7 +3,7 @@ package com.insac.can.livebus.core
 class LiveBus {
     companion object {
         private var mInstance: LiveBus? = null
-        private var mEvents: HashMap<String, LiveEventBase<Any>> = HashMap()
+        private var mEvents: HashMap<String, LiveEventBase<out Any?>> = HashMap()
 
         fun getInstance(): LiveBus {
             if (mInstance == null) {
@@ -21,8 +21,25 @@ class LiveBus {
             // TODO implement remove event method
         }
 
-        fun <T> broadcastEvent(tag: String, eventValue: T) {
-            // TODO implement publish  event method
+        fun <T> postLiveEvent(tag: String, eventValue: T) {
+            if (!mEvents.contains(tag)) {
+                val liveEvent = LiveEvent<T>()
+                mEvents[tag] = liveEvent
+            }
+
+            mEvents[tag]?.value = eventValue
+        }
+
+        fun <T> postSingleEvent(tag: String, eventValue: T) {
+
+        }
+
+        fun <T> postStickySingleEvent(tag: String, eventValue: T) {
+
+        }
+
+        fun <T> postStickyEven(tag: String, eventValue: T) {
+
         }
 
         fun removeStickyEvent(tag: String) {
