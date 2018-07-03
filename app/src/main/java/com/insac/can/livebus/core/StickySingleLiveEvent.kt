86fba02c9f7f5d218da03a134e81d9b830a16c6f
Can Undeger <canundeger@gmail.com>
2018-07-03 00:03:50 -0400

    Implement StickSingleLiveEvent
    - This event will be sent to first observer
    that starts to observe the event. If there is no
    observer available it won't be removed from the bus until
    it is consumed.

--- app/src/main/java/com/insac/can/livebus/core/StickySingleLiveEvent.kt
+++ app/src/main/java/com/insac/can/livebus/core/StickySingleLiveEvent.kt
@@ -1,6 +1,42 @@
 package com.insac.can.livebus.core
 
-import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.Observer
+import android.support.annotation.MainThread
+import android.support.annotation.Nullable
+import android.util.Log
+import java.util.concurrent.atomic.AtomicBoolean
 
-class StickySingleLiveEvent<T> : LiveEvent<T>() {
+class StickySingleLiveEvent<T> : LiveEventBase<T>() {
+    private val TAG = "SingleLiveEvent"
+    private val mPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
+        if (hasActiveObservers()) {
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        }
+
+        super.observe(owner, Observer {
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(it)
+            }
+        })
+    }
+
+    @MainThread
+    override fun setValue(@Nullable t: T?) {
+        mPending.set(true)
+        super.setValue(t)
+    }
+
+    /**
+     * Used for cases where T is Void, to make calls cleaner.
+     */
+    @MainThread
+    fun call() {
+        value = null
+    }
+
+    fun isConsumed() : Boolean = mPending.get()
 }
