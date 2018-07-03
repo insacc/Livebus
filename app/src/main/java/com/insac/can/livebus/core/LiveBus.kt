b8c589b7fbb57bdaa045fcd6b5de667e7db5c4b8
Can Undeger <canundeger@gmail.com>
2018-07-02 23:58:02 -0400

    Update LiveBus component

--- app/src/main/java/com/insac/can/livebus/core/LiveBus.kt
+++ app/src/main/java/com/insac/can/livebus/core/LiveBus.kt
@@ -3,7 +3,7 @@ package com.insac.can.livebus.core
 class LiveBus {
     companion object {
         private var mInstance: LiveBus? = null
-        private var mEvents: HashMap<String, LiveEvent<Any>> = HashMap()
+        private var mEvents: HashMap<String, LiveEventBase<Any>> = HashMap()
 
         fun getInstance(): LiveBus {
             if (mInstance == null) {
@@ -13,16 +13,20 @@ class LiveBus {
             return mInstance!!
         }
 
-        private fun <T> addEvent(liveEvent: LiveEvent<T>) {
+        private fun <T> addEvent(liveEventBase: LiveEventBase<T>) {
             // TODO implement add event method
         }
 
-        fun <T> removeEvent(liveEvent: LiveEvent<T>) {
+        fun <T> removeEvent(liveEventBase: LiveEventBase<T>) {
             // TODO implement remove event method
         }
 
         fun <T> broadcastEvent(tag: String, eventValue: T) {
             // TODO implement publish  event method
+        }
+
+        fun removeStickyEvent(tag: String) {
+            // TODO implement remove sticky event
         }
     }
 }
