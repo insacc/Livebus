e13cc3b7a35495c3eef5fa6ee6fe255748f34f3a
Can Undeger <canundeger@gmail.com>
2018-07-02 23:59:28 -0400

    Create LiveEvent
    - This event will be deleted once there is no
    observer left.

--- app/src/main/java/com/insac/can/livebus/core/LiveEvent.kt
+++ app/src/main/java/com/insac/can/livebus/core/LiveEvent.kt
@@ -1,20 +1,8 @@
 package com.insac.can.livebus.core
 
-import android.arch.lifecycle.LifecycleOwner
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
 
-open class LiveEvent<T> : MutableLiveData<T>() {
-    private var mPendingObserve: Boolean = false
-
-    override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
-        // Let the same observer to register once
-        mPendingObserve = true
-        removeObserver(observer)
-        mPendingObserve = false
-        super.observe(owner, observer)
-    }
-
+class LiveEvent<T> : LiveEventBase<T>() {
     override fun removeObserver(observer: Observer<T>) {
         super.removeObserver(observer)
         // Remove from bus if there are no observer left
