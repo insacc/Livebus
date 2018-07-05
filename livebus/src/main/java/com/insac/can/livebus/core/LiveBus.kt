581422bb24bf645f0057a66b9178762b62f6eb25
Can Undeger <canundeger@gmail.com>
2018-07-05 00:00:12 -0400

    Add subscribe to events functions

--- livebus/src/main/java/com/insac/can/livebus/core/LiveBus.kt
+++ livebus/src/main/java/com/insac/can/livebus/core/LiveBus.kt
@@ -75,4 +75,53 @@ class LiveBus {
 
         mEvents.remove(tag)
     }
+
+    fun subscribeEvent(tag: String): LiveEventBase<out Any?>? {
+        if (mEvents.containsKey(tag)) {
+            return mEvents[tag]
+        }
+
+        return null
+    }
+
+    fun <T> subscribeLiveEvent(tag: String, type: Class<T>): LiveEventBase<T> {
+        if (mEvents.containsKey(tag)) {
+            return mEvents[tag] as LiveEvent<T>
+        } else {
+            val liveEvent = LiveEvent<T>()
+            mEvents.put(tag, liveEvent)
+            return liveEvent
+        }
+    }
+
+    fun <T> subscribeSingleLiveEvent(tag: String, type: Class<T>): LiveEventBase<T> {
+        if (mEvents.containsKey(tag)) {
+            return mEvents[tag] as SingleLiveEvent<T>
+        } else {
+            val liveEvent = SingleLiveEvent<T>()
+            mEvents[tag] = liveEvent
+            return liveEvent
+        }
+    }
+
+    fun <T> subscribeStickyLiveEvent(tag: String, type: Class<T>): LiveEventBase<T> {
+        if (mEvents.containsKey(tag)) {
+            return mEvents[tag] as StickyLiveEvent<T>
+        } else {
+            val liveEvent = StickyLiveEvent<T>()
+            mEvents[tag] = liveEvent
+            return liveEvent
+        }
+    }
+
+    fun <T> subscribeStickySingleLiveEvent(tag: String, type: Class<T>): LiveEventBase<T> {
+        if (mEvents.containsKey(tag)) {
+            return mEvents[tag] as StickySingleLiveEvent<T>
+        } else {
+            val liveEvent = StickySingleLiveEvent<T>()
+            mEvents[tag] = liveEvent
+            return liveEvent
+        }
+    }
+
 }
