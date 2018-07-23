3e118c3e11c614552038d17dd8e4a40146f49192
Can Undeger <canundeger@gmail.com>
2018-07-23 01:07:59 -0400

    Add different exception wrapper functions

--- livebus/src/main/java/com/insac/can/livebus/utils/ExceptionUtil.kt
+++ livebus/src/main/java/com/insac/can/livebus/utils/ExceptionUtil.kt
@@ -18,3 +18,30 @@ fun <T> exceptionWrapper(func: () -> T, defaultValue: T): T {
         defaultValue
     }
 }
+
+fun <T> exceptionWrapper(func: () -> T): T {
+    return try {
+        func()
+    } catch (ex: Exception) {
+        logExceptionOnlyInDebug(ex)
+        throw LiveBusException(ex.toString())
+    }
+}
+
+fun <T> exceptionWrapper(func: () -> T, exceptionMessage: String): T {
+    return try {
+        func()
+    } catch (ex: Exception) {
+        logExceptionOnlyInDebug(ex)
+        throw LiveBusException(exceptionMessage)
+    }
+}
+
+fun exceptionWrapper(func: () -> Unit, exceptionMessage: String) {
+    try {
+        func()
+    } catch (ex: Exception) {
+        logExceptionOnlyInDebug(ex)
+        throw LiveBusException(exceptionMessage)
+    }
+}
