b796896ec3f6a08165c84304547faf7398ffc041
Can Undeger <canundeger@gmail.com>
2018-10-07 17:43:44 -0400

    Send values from background thread to test

--- sample/src/main/java/com/insac/can/sample/MainActivity.kt
+++ sample/src/main/java/com/insac/can/sample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.insac.can.livebus.core.LiveBus
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +30,33 @@ class MainActivity : AppCompatActivity() {
         sticky_live_event_button.setOnClickListener { it ->
             it?.let {
                 LiveBus.getInstance().setStickyLiveEventValue("StickyLiveEvent", "TestLiveEvent")
+                openSecondActivity()
+            }
+        }
+
+        bg_live_event_button.setOnClickListener { it ->
+            it?.let {
+                doAsync {
+                    LiveBus.getInstance().postLiveEventValue("LiveEventBG", "TestLiveEvent")
+                }
+                openSecondActivity()
+            }
+        }
+
+        bg_single_live_event_button.setOnClickListener { it ->
+            it?.let {
+                doAsync {
+                    LiveBus.getInstance().postSingleLiveEventValue("SingleLiveEventBG", "TestLiveEvent")
+                }
+                openSecondActivity()
+            }
+        }
+
+        bg_sticky_live_event_button.setOnClickListener { it ->
+            it?.let {
+                doAsync {
+                    LiveBus.getInstance().postStickyLiveEventValue("StickyLiveEventBG", "TestLiveEvent")
+                }
                 openSecondActivity()
             }
         }
