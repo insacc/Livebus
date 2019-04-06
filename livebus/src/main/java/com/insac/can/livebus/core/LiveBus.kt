351e8cc672781600b58922184b3634ef78ceb927
Can Undeger <canundeger@gmail.com>
2019-04-06 14:45:45 -0400

    Use assert func from Util file instead

--- livebus/src/main/java/com/insac/can/livebus/core/LiveBus.kt
+++ livebus/src/main/java/com/insac/can/livebus/core/LiveBus.kt
@@ -1,7 +1,6 @@
 package com.insac.can.livebus.core
 
-import android.os.Looper
-import com.insac.can.livebus.utils.LiveBusException
+import com.insac.can.livebus.utils.assertMainThread
 import com.insac.can.livebus.utils.exceptionWrapper
 
 class LiveBus {
@@ -321,13 +320,6 @@ class LiveBus {
             val liveEvent = StickySingleLiveEvent<T>()
             mEvents[tag] = liveEvent
             liveEvent
-        }
-    }
-
-    private fun assertMainThread(methodName: String) {
-        if (Looper.myLooper() == Looper.getMainLooper()) {
-            throw LiveBusException("Cannot invoke " + methodName + " on a background"
-                    + " thread")
         }
     }
 }
